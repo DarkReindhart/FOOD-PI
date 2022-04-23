29c22b506bec70eb5d10873170031d3b4505c8bf
Renato Zapata <renatozapgon@gmail.com>
2022-04-23 12:59:50 -0400

    Modelos y Conexion a DB

--- PI-Food-main/api/src/models/Recipe.js
+++ PI-Food-main/api/src/models/Recipe.js
@@ -3,10 +3,44 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define('recipe', { 
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    }, 
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    score: {
+      type: DataTypes.INTEGER
+    },
+    healtScore: {
+      type: DataTypes.INTEGER
+    },
+    steps: {
+      type: DataTypes.TEXT // o ARRAY? ver que es mejor. 
+    },
+    image: {
+      type: DataTypes.STRING,
+      validate: {
+        isURL: true,
+        isValidFormat(value){
+          let validate1 = value.split(".")
+          let finalValidator = validate1[validate1.length - 1]
+          const imageTypes = ["jpg", "jpeg", "gif", "png"]
+
+          if(!imageTypes.includes(finalValidator)){
+            throw new Error(' El formato de imagen no es válido.')
+          }
+        }
+      }
+    }
+   }, {timestamps: true});
 };
